ff9a909c027a3d30533b8f9e2109f8e0ffb61828
mohamed abdelhady <abdelhadymohamad1@gmail.com>
2023-05-07 09:28:59 -0400

    stuff yeah

---
+++ MasseyHacks-2023/Assets/scripts/sword.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sword : MonoBehaviour
+{
+    [SerializeField]
+    float damage = 5f;
+
+    public Camera cam;
+    public Rigidbody2D rb;
+    public Animator anim;
+    public LayerMask enemy;
+    public Transform attackPoint;
+    public float range = 0.5f;
+
+    Vector2 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+
+    void Attack()
+    {
+        anim.SetTrigger("attack");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, range, enemy);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit" + enemy.name);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, range);
+    }
+}
